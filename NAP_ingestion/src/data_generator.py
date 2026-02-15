a5c53d36661cc74d4eb14b316ed2a70391e8e343
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2026-02-15 12:18:09 -0500

    basic generation working

--- NAP_ingestion/src/data_generator.py
+++ NAP_ingestion/src/data_generator.py
@@ -8,6 +8,14 @@ import pandas as pd
 random.seed(42)
 np.random.seed(42)
 
+# ======= HARDCODED INFO ========= #
+
+companies = [
+    {"id": "COMP1", "currency": "USD"},
+    {"id": "COMP2", "currency": "AED"}
+]
+
+# ======= TIME-BASED FUNCTIONS ====== #
 
 def generate_fx_series(
     start_time,
@@ -39,11 +47,44 @@ def generate_fx_series(
         "rate": rates
     })
 
+def generate_transactions(n, start_time, end_time):
+    rows = []
+
+    for _ in range(n):
+        company = random.choice(companies)
+
+        event_time = start_time + timedelta(
+            seconds=random.randint(0, int((end_time - start_time).total_seconds()))
+        )
+
+        base_cncy = random.choice(["USD", "EUR", "AED"])
+        quote_cncy = company["currency"]
+
+        base_amount = round(random.uniform(10, 500), 2)
+
+        fee = round(base_amount * 0.01, 2)
+
+        rows.append({
+            "event_timestamp": event_time,
+            "company_id": company["id"],
+            "base_cncy": base_cncy,
+            "quote_cncy": quote_cncy,
+            "base_amount": base_amount,
+            "fee_amount": fee
+        })
+
+    return pd.DataFrame(rows)
+
+# ====== MAIN ===== #
+
 if __name__ == "__main__":
     s_time = datetime.now()
-    e_time = s_time + timedelta(seconds=10)
+    e_time = s_time + timedelta(minutes=10)
     base = 'USD'
     quote = 'AED'
     s_rate = 3.67
-    frame = generate_fx_series(s_time, e_time, base, quote, s_rate)
-    print(frame)
+    fx_frame = generate_fx_series(s_time, e_time, base, quote, s_rate)
+    tx_frame = generate_transactions(30, s_time, e_time)
+    print(fx_frame.head())
+    print(tx_frame.head())
+    print(tx_frame["base_cncy"].value_counts())
